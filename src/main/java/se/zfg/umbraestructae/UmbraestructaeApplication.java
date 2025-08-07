195a47297a6741c23108c5bad57b9aa15d4e5d5e
NEXUSGROUP\magnus.ahlden <magnus.ahlden@nexusgroup.com>
2025-08-07 12:13:09 +0200

    no message

---
+++ src/main/java/se/zfg/umbraestructae/UmbraestructaeApplication.java
@@ -0,0 +1,13 @@
+package se.zfg.umbraestructae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbraestructaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbraestructaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/zfg/umbraestructae/UmbraestructaeApplicationTests.java
@@ -0,0 +1,13 @@
+package se.zfg.umbraestructae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbraestructaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
